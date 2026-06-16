af1280d35aa0a35bdea215907709af0c779ac36c
Taroom <T4room@yandex.ru>
2026-06-16 23:10:30 +0300

    Fix code style again

--- src/main/java/ru/yandex/practicum/sleeptracker/functions/core/SleepTypes.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/functions/core/SleepTypes.java
@@ -4,5 +4,4 @@ public enum SleepTypes {
     LARK,
     OWL,
     PIGEON
-
 }
--- src/main/java/ru/yandex/practicum/sleeptracker/service/SleepingSessionService.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/service/SleepingSessionService.java
@@ -9,16 +9,14 @@ import static ru.yandex.practicum.sleeptracker.customExceptions.Logger.logging;
 
 public class SleepingSessionService {
 
-    public static List<SleepingSession> getSleepingSessionList(List<String> sleepingList) {
-        return sleepingList.stream()
-                .map(s -> {
-                    String[] parts = s.split(";");
-                    logging("Элемент " + Arrays.toString(parts));
-                    return new SleepingSession(
-                            parts[0],
-                            parts[1],
-                            QualityOfSleep.valueOf(parts[2]))
-                ;})
-                .collect(Collectors.toList());
-    }
+  public static List<SleepingSession> getSleepingSessionList(List<String> sleepingList) {
+    return sleepingList.stream()
+        .map(
+            s -> {
+              String[] parts = s.split(";");
+              logging("Элемент " + Arrays.toString(parts));
+              return new SleepingSession(parts[0], parts[1], QualityOfSleep.valueOf(parts[2]));
+            })
+        .collect(Collectors.toList());
+  }
 }
